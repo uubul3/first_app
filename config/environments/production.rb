b9f8890e4018dadc246df5dc70f63dc3f268cc5a
uubul3 <uubul3@gmail.com>
2014-04-08 22:57:41 +0200

    Update Gemfile.lock for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,3 +78,9 @@ FirstApp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
+
+group :production do
+ gem 'pg', '0.15.1'
+ gem 'rails_12factor', '0.0.2'
+end
+
